2797c31b2d37d92f9a4d7b37aece1e0965733646
Altynai <ianytlaa@gmail.com>
2024-05-08 01:55:38 +0500

    прописала переменные которые позже буду использовать

---
+++ src/index.js
@@ -0,0 +1,20 @@
+let size = 10; 
+let bombFrequency = 0.2; 
+let tileSize = 50;
+
+const board = document.querySelectorAll('.board')[0];
+let tiles;
+let boardSize;
+
+const restartBtn = document.querySelectorAll('.minesweeper-btn')[0];
+const endscreen = document.querySelectorAll('.endscreen')[0]
+
+let bombs = [];
+let numbers = [];
+let numberColors = ['#3498db', '#2ecc71', '#e74c3c', '#9b59b6', '#f1c40f', '#1abc9c', '#34495e', '#7f8c8d',];
+let endscreenContent = {
+    win: 'ура пабеда', 
+    loose: 'ты лох'
+};
+
+let gameOver = false;
